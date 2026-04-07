2e31890774867a65d0e9d94e16a017878fa8c58e
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2026-04-07 19:51:30 -0400

    Add .gitignore, .python-version, update game_state.jsonl, fix logger.py, and enhance main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from logger import log_state
 import pygame
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids...")
     while True:
@@ -12,6 +14,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = (pygame.time.Clock().tick(60)) / 1000
 if __name__ == "__main__":
     main()
