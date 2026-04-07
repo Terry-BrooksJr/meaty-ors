0f2b9aba4e3e5bf17a3f5bca75971b9116d38cf2
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2026-04-07 19:38:00 -0400

    Init Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+import pygame
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids...")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
