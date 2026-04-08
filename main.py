b9551df1a44ee0857993064019123f883c073745
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2026-04-08 08:51:46 -0400

    Refactor game logic to utilize sprite groups for player updates and enhance movement controls

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 import pygame
 from player import Player
+
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable )
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids...")
     while True:
@@ -15,10 +21,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
+   
